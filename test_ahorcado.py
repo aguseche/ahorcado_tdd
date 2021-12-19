3dd5b8874ad51468d1c54d7ac9498deec4dd9645
DamianBarzola <dbdamix@gmail.com>
2021-12-19 18:32:13 -0300

    tests running

--- test_ahorcado.py
+++ test_ahorcado.py
@@ -1,38 +1,47 @@
 import unittest
 from api.logic.ahorcado_logic import Ahorcado
+from fastapi import HTTPException
 
-# class TestLogin(unittest.TestCase):
-    # def test_nombre_vacio(self):
-    #     ahorcado = Ahorcado()
-    #     with self.assertRaises(Exception) as context:    
-    #         ahorcado.login('')
-    #     self.assertTrue('Debe ingresar un nombre' in str(context.exception))
+class TestLogin(unittest.TestCase):
+    def test_nombre_vacio(self):
+        ahorcado = Ahorcado()
+        try:
+            ahorcado.login('')
+        except HTTPException as e:
+            self.assertTrue('Debe ingresar un nombre',e.detail)
     
-    # def test_maxlen_25(self):
-    #     ahorcado = Ahorcado()
-    #     with self.assertRaises(Exception) as context:    
-    #         ahorcado.login('holamellamoagustinetcheverry')
-    #     self.assertTrue('El nombre no debe tener mas de 25 caracteres' in str(context.exception))
+    def test_maxlen_25(self):
+        ahorcado = Ahorcado()
+        try:  
+            ahorcado.login('holamellamoagustinetcheverry')
+        except HTTPException as e:
+            self.assertTrue('El nombre no debe tener mas de 25 caracteres',e.detail)
+   
+    def test_minlen_3(self):
+        ahorcado = Ahorcado()
+        try:   
+            ahorcado.login('xd')
+        except HTTPException as e: 
+            self.assertTrue('El nombre debe tener mas de 2 caracteres',e.detail)
 
-    # def test_minlen_3(self):
-    #     ahorcado = Ahorcado()
-    #     with self.assertRaises(Exception) as context:    
-    #         ahorcado.login('xd')
-    #     self.assertTrue('El nombre debe tener mas de 2 caracteres' in str(context.exception))
-    # def test_caracteres_especiales(self):
-    #     ahorcado = Ahorcado()
-    #     with self.assertRaises(Exception) as context:    
-    #         ahorcado.login('@3hola')
-    #     self.assertTrue('El nombre no puede tener caracteres especiales o espacios' in str(context.exception))
-    # def test_espacios_vacios(self):
-    #     ahorcado = Ahorcado()
-    #     with self.assertRaises(Exception) as context:    
-    #         ahorcado.login('Agustin Etcheverry')
-    #     self.assertTrue('El nombre no puede tener caracteres especiales o espacios' in str(context.exception))
-    # def test_getName(self):
-    #     ahorcado = Ahorcado()
-    #     ahorcado.login('Agustin')
-    #     self.assertEqual('Agustin',ahorcado.get_name())
+    def test_caracteres_especiales(self):
+        ahorcado = Ahorcado()
+        try:   
+            ahorcado.login('@3hola')
+        except HTTPException as e: 
+            self.assertTrue('El nombre no puede tener caracteres especiales o espacios',e.detail)
+    
+    def test_espacios_vacios(self):
+        ahorcado = Ahorcado()
+        try:   
+            ahorcado.login('Agustin Etcheverry')
+        except HTTPException as e: 
+            self.assertTrue('El nombre no puede tener caracteres especiales o espacios',e.detail)
+    
+    def test_getName(self):
+        ahorcado = Ahorcado()
+        ahorcado.login('Agustin')
+        self.assertEqual('Agustin',ahorcado.get_name())
 
 class TestJuego(unittest.TestCase):
     def test_inicializarVariables(self):

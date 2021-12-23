8b8d261e4df8fb4d7453216c7b485bb1ddaead51
DamianBarzola <dbdamix@gmail.com>
2021-12-22 22:33:15 -0300

    first AT

--- features/guessletter.feature
+++ features/guessletter.feature
@@ -3,15 +3,13 @@ Feature: Guess letter
   Scenario: Guess a letter correctly
     Given I set damian as name
     And Start a game
-    And Insert letter
+    When Insert letter a
     And Try letter
-    When Letter is correct
     Then Letter shows in word
 
   Scenario: Guess a letter incorrectly
     Given I set damian as name
     And Start a game
-    And Insert letter
+    When Insert letter x
     And Try letter
-    When Letter is incorrect
     Then Letter shows in incorrect array
--- features/steps/guessletter.py
+++ features/steps/guessletter.py
@@ -1,27 +1,51 @@
 from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+options =  webdriver.ChromeOptions()
+
+driver_path = 'C:/Users/Usuario/py/Agiles/ahorcado_tdd/features/chromedriver.exe'
+driver = webdriver.Chrome(driver_path, chrome_options=options)
+driver.implicitly_wait(15)
 
 @given('I set damian as name')
-def step_impl(context):
-    pass
+def step_impl(context):  
+    driver.get('http://localhost:3000/')
+    input=driver.find_element_by_id("name")
+    input.send_keys("damian")
 
 @given('Start a game')
-def step_impl(context):
-    pass
+def step_impl(context):    
+    driver.find_element_by_id("loginBtn").click()
 
-@given('Insert letter')
-def step_impl(context):
-    pass
+@when('Insert letter a')
+def step_impl(context):    
+    inputLetter= driver.find_element_by_id("inputLetter")
+    inputLetter.send_keys("a")
 
-@given('Try letter')
-def step_impl(context):
-    pass
-
-@when('Letter is correct')
-def step_impl(context):
-    pass
+@when('Try letter')
+def step_impl(context): 
+    driver.find_element_by_id("btnTry").click()
 
 @then('Letter shows in word')
 def step_impl(context):
-    pass
+    msg= driver.find_element_by_id("palabra").text
+    assert "a" in msg    
+
+@when('Insert letter x')
+def step_impl(context):    
+    inputLetter= driver.find_element_by_id("inputLetter")
+    inputLetter.send_keys("x")
+
+
+@then('Letter shows in incorrect array')
+def step_impl(context):
+    msg= driver.find_element_by_id("letrasIncorrectas").text
+    assert "x" in msg    
+    driver.quit()
+    

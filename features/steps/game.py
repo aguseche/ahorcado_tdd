11f902d5acccea029f9029480a3ab6f6d504cc0c
DamianBarzola <dbdamix@gmail.com>
2022-02-27 18:06:28 -0300

    .

--- features/steps/game.py
+++ features/steps/game.py
@@ -2,16 +2,35 @@ from behave import *
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
 
 # options =  webdriver.ChromeOptions()
-
-driver_path = 'features/chromedriver.exe'
-driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+# driver_path = '../chromedriver.exe'
+# driver = webdriver.Chrome(driver_path, chrome_options=options)
 driver.implicitly_wait(15)
 
 @given('I set giova as name')
 def step_impl(context):  
-    driver.get('https://ahorcado-agiles.netlify.app/')
+    driver.get('http://localhost:3000/')
     input=driver.find_element_by_id("name")
     input.send_keys("giova")
 
